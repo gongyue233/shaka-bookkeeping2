b074e364d7f8bc4a43d7f9b8b1471db67911831c
baiyang <gongyue233@163.com>
2021-05-26 12:31:45 +0800

    deploy

---
+++ precache-manifest.424e563385f0f219c27e58d309268dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c5f325695093f8c2d0",
+    "url": "/shaka-bookkeeping2/css/app.7e44e119.css"
+  },
+  {
+    "revision": "6220989e01f5fb51a6d3d40a44bc64cd",
+    "url": "/shaka-bookkeeping2/index.html"
+  },
+  {
+    "revision": "10c5f325695093f8c2d0",
+    "url": "/shaka-bookkeeping2/js/app.4a5e4b62.js"
+  },
+  {
+    "revision": "187e454af05056b9f78c",
+    "url": "/shaka-bookkeeping2/js/chunk-vendors.c16b3798.js"
+  },
+  {
+    "revision": "dac470dcef717e49db8ee49753e42122",
+    "url": "/shaka-bookkeeping2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shaka-bookkeeping2/robots.txt"
+  }
+]);
